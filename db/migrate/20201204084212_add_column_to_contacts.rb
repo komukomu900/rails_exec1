7970eb294e7c6b1d699860846ee40bdaee82e59d
komukomu900 <z9l2c26@gamil.com>
2020-12-08 19:39:10 +0900

    first

---
+++ db/migrate/20201204084212_add_column_to_contacts.rb
@@ -0,0 +1,9 @@
+class AddColumnToContacts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contents do |t|
+      t.string :name
+      t.string :email
+      t.text :content
+    end
+  end
+end
